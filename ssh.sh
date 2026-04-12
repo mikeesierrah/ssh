9df49ca46e13795823c924e0cdf96217b305d1b4
Mehrad <144580664+mikeesierrah@users.noreply.github.com>
2026-04-12 08:32:46 +0300

    Improve SSH key installation
    Refactor SSH key installation to avoid duplicates and ensure proper permissions.

--- ssh.sh
+++ ssh.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Change Here
 SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIv6hCWuGyj9JIWrzxOQErRw9pELRu+xuOND/ZSYZe+J mike@key"
 
+mkdir -p ~/.ssh && chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys
 
-mkdir -p ~/.ssh
-echo "$SSH_KEY" >> ~/.ssh/authorized_keys
-
-
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
-
-echo "SSH key installed successfully."
+grep -qxF "$SSH_KEY" ~/.ssh/authorized_keys || {
+    tail -c1 ~/.ssh/authorized_keys | read -r _ || echo >> ~/.ssh/authorized_keys
+    echo "$SSH_KEY" >> ~/.ssh/authorized_keys
+}
